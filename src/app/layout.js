7361b3f1e48d668e9ca4dbb7696f1258d1b5baac
Muhammad Ali <muhammadaliweb4@gmail.com>
2026-05-06 09:05:29 +0500

    Making changes and adding new way for google tags manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,6 +33,21 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${montserrat.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-53FRP5H5');
+    `,
+          }}
+        />
+      </head>
       <body className="min-h-full flex flex-col">
         {/* Google Ads Global Site Tag */}
         <Script
@@ -40,7 +55,10 @@ export default function RootLayout({ children }) {
           strategy="afterInteractive"
         />
 
-        <Script id="google-ads-init" strategy="afterInteractive">
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-53FRP5H5"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
+        {/* <Script id="google-ads-init" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
@@ -48,7 +66,7 @@ export default function RootLayout({ children }) {
             gtag('config', 'AW-11557520972');
           `}
         </Script>
-        <AdsPageTracker />
+        <AdsPageTracker /> */}
         {children}
       </body>
     </html>
