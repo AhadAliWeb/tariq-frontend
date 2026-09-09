44003d3ff40c3f31a806d0dc641dd18c83045cff
Muhammad Ali <muhammadaliweb4@gmail.com>
2026-09-09 20:49:08 +0500

    made few changes in home page

--- src/app/page.js
+++ src/app/page.js
@@ -58,9 +58,9 @@ export default function RootLayout({ children }) {
       <AboutComponent />
       <ContactForm />
       <WhyChooseUs />
+      <HowItWorks STEPS={STEPS}/>
       <CtaBanner />
       <ExploreCourses />
-      <HowItWorks STEPS={STEPS}/>
       <WhyStudyWithUs />
       <WhatPeopleSay />
       <StudentTestimonials />
