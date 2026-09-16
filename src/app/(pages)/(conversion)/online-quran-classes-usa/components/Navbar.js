f8a6f45d5a6afac681f20034ee2b7f43653b2942
Muhammad Ali <muhammadaliweb4@gmail.com>
2026-09-16 21:45:53 +0500

    Fix image path in navbar

--- src/app/(pages)/(conversion)/online-quran-classes-usa/components/Navbar.js
+++ src/app/(pages)/(conversion)/online-quran-classes-usa/components/Navbar.js
@@ -144,7 +144,7 @@ export default function Navbar() {
         {/* Logo */}
         <Link href="/" className="flex items-center gap-2.5 shrink-0 group">
           <Image
-            src="/images/Logo.png"
+            src="/images/logo.png"
             alt="Logo"
             width="100"
             height="100"
