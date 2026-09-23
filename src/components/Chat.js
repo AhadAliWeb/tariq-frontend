fe207fe214a0dd4099287dbf65f90af918b93d07
Muhammad Ali <muhammadaliweb4@gmail.com>
2026-09-23 08:59:08 +0500

    changes tawk.to script

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -9,7 +9,7 @@ export default function Chat() {
           (function(){
           var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
           s1.async=true;
-          s1.src='https://embed.tawk.to/69f80a4651b2bd1c35226718/1jnoeg9dv';
+          s1.src='https://embed.tawk.to/6ab32c1bcc78583443ace295/default';
           s1.charset='UTF-8';
           s1.setAttribute('crossorigin','*');
           s0.parentNode.insertBefore(s1,s0);
